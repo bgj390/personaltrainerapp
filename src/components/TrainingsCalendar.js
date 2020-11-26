749b897615b4dbb202535832b65b8b4e7d431570
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-11-26 17:38:53 +0200

    titles for calendar added

--- src/components/TrainingsCalendar.js
+++ src/components/TrainingsCalendar.js
@@ -1,36 +1,58 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-const localizer = momentLocalizer(moment)
  
-class TrainingsCalendar extends Component {
-    state = {
-      events: [
-        {
-          start: moment().toDate(),
-          end: moment()
-            .add(1, "days")
-            .toDate(),
-          title: "Some title"
+function TrainingsCalendar() {
+
+    const localizer = momentLocalizer(moment)
+    const [trainings, setTrainings] = useState([])
+
+    useEffect(() => {
+      getTrainings()
+    });
+
+    const getTrainings = () => {
+      fetch('https://customerrest.herokuapp.com/gettrainings')
+        .then(response => response.json())
+        .then(data => setTrainings(data.map((training) => (
+            {
+                title: training.activity,
+                start: moment.utc(training.date)._d,
+                end: moment.utc(training.date).add(training.duration, 'minutes')._d,
+                resource: training.customer.id
+            })
+        )))
+        .catch(err => console.error(err))
+    }
+/*
+    const events = trainings.map((training) => {
+        return {
+          id: training.id,
+          title: training.activity,
+          duration: training.duration,
+          firstname: training.customer.firstname,
+          lastname: training.customer.lastname,
+          start: new Date(training.date),
+          end: new Date(training.date),
+          allDay: false
         }
-      ]
-    };
-  
-    render() {
+      })
+*/  
       return (
         <div className="TrainingsCalendar">
           <Calendar
             localizer={localizer}
             defaultDate={new Date()}
-            defaultView="month"
-            events={this.state.events}
+            defaultView='month'
+            events={trainings}
+            resourceAccessor='resource'
+            startAccessor='start'
+            endAccessor='end'
             style={{ height: "100vh" }}
           />
         </div>
       );
-    }
   }
   
   export default TrainingsCalendar;
