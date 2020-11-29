8685a7567db7c2ad26f95cb90bbdf4710609ed14
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-11-29 13:26:13 +0200

    deployment settings added

---
+++ server.js
@@ -0,0 +1,14 @@
+// https://stackoverflow.com/questions/59850705/how-to-deploy-react-application-to-heroku
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
